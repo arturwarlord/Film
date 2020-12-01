c3ff17434fcb619647fb8452dca2764a1f475b67
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2020-12-01 14:58:37 +0500

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,19 @@
 console.log("tabs");
 console.log("modal");
 
+new Swiper('.film-slider', {
+    loop: true,
 
+    speed: 1000,
 
+    autoplay: true,
+});
 
 $(function(){
     $(".header__search").click(function(){
         $(".header-search").slideToggle(1000);
     });
 });
-
-
 
 
 
@@ -26,10 +29,21 @@ function myFunction(x) {
 
 
 
-new Swiper('.film-slider', {
-    loop: true,
 
-    speed: 1000,
 
-    autoplay: true,
+
+$(function(){
+    $(".flip__inner .search__ico").click(function(){
+        $(".box").toggleClass('show');
+        $(".first__mobile").toggleClass('orbit');
+        $(".back__mobile").toggleClass('dirol');
+    });
+});
+
+$(function(){
+    $(".flip__bottom .close").click(function(){
+        $(".box").toggleClass('show');
+        $(".first__mobile").removeClass('orbit');
+        $(".back__mobile").removeClass('dirol');
+    });
 });
